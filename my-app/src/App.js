b3560c9b302788ce0c9ccf700f3e79e470acdba0
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-11-08 16:20:24 +0800

    Add lesson 12

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
+        <p>Welcome :)</p>
+        <Ninjas />
       </div>
     );
   }
---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+  render() {
+    return (
+      <div className="ninjas">
+        <div>Name: Ryu</div>
+        <div>Age: 30 </div>
+        <div>Bet: black </div>
+      </div>
+    )
+  }
+}
+
+export default Ninjas;
