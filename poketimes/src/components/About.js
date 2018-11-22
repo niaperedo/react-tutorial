54a78cced7a38c052736b7c07c2aa23085cab97f
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-11-22 17:15:27 +0800

    Add lesson 25

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Matcha is finely ground powder specially grown and processed green tea leaves. It is special in two aspects of farming and processing: the green tea plants for matcha are shade-grown for about three weeks before harvest and the stems and veins are removed in processing.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Matcha is finely ground powder specially grown and processed green tea leaves. It is special in two aspects of farming and processing: the green tea plants for matcha are shade-grown for about three weeks before harvest and the stems and veins are removed in processing.
+      </p>
+    </div>
+  )
+}
+
+export default Home
