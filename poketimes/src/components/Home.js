40aa2bd3e066a92df8f326c1b082bbb296bfde7b
Adrianne Peredo <aperedo@uth702@gmail.com>
2019-01-08 14:58:00 +0800

    Add lesson 40

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-  state = {
-    posts: []
-  }
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(resp => {
-        this.setState({
-          posts: resp.data.slice(0, 10)
-        })
-    })
-  }
   render() {
-    const {posts} = this.state
+    const {posts} = this.props
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -42,4 +33,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
